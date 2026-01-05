9233ed5ab6838a06955b0cace153e0c57b3c9318
Do-EE2-IoT <do30032003.com>
2026-01-05 14:01:32 +0700

    feat: increase gain for mic

--- projects/media/audio_record_to_sdcard/main/audio_record.c
+++ projects/media/audio_record_to_sdcard/main/audio_record.c
@@ -20,80 +20,79 @@
 #include "ff.h"
 #include "diskio.h"
 
-
-#define TAG  "AUD_RECORD_SDCARD"
+#define TAG "AUD_RECORD_SDCARD"
 
 static FIL mic_file;
 static char mic_file_name[50];
 
 static FATFS *pfs = NULL;
 
-
 static bk_err_t tf_mount(void)
 {
-	FRESULT fr;
+    FRESULT fr;
 
-	if (pfs != NULL)
-	{
-		os_free(pfs);
-	}
+    if (pfs != NULL)
+    {
+        os_free(pfs);
+    }
 
-	pfs = os_malloc(sizeof(FATFS));
-	if(NULL == pfs)
-	{
-		BK_LOGI(TAG, "f_mount malloc failed!\r\n");
-		return BK_FAIL;
-	}
+    pfs = os_malloc(sizeof(FATFS));
+    if (NULL == pfs)
+    {
+        BK_LOGI(TAG, "f_mount malloc failed!\r\n");
+        return BK_FAIL;
+    }
 
-	fr = f_mount(pfs, "1:", 1);
-	if (fr != FR_OK)
-	{
-		BK_LOGE(TAG, "f_mount failed:%d\r\n", fr);
-		return BK_FAIL;
-	}
-	else
-	{
-		BK_LOGI(TAG, "f_mount OK!\r\n");
-	}
+    fr = f_mount(pfs, "1:", 1);
+    if (fr != FR_OK)
+    {
+        BK_LOGE(TAG, "f_mount failed:%d\r\n", fr);
+        return BK_FAIL;
+    }
+    else
+    {
+        BK_LOGI(TAG, "f_mount OK!\r\n");
+    }
 
-	return BK_OK;
+    return BK_OK;
 }
 
 static bk_err_t tf_unmount(void)
 {
-	FRESULT fr;
-	fr = f_unmount(DISK_NUMBER_SDIO_SD, "1:", 1);
-	if (fr != FR_OK)
-	{
-		BK_LOGE(TAG, "f_unmount failed:%d\r\n", fr);
-		return BK_FAIL;
-	}
-	else
-	{
-		BK_LOGI(TAG, "f_unmount OK!\r\n");
-	}
+    FRESULT fr;
+    fr = f_unmount(DISK_NUMBER_SDIO_SD, "1:", 1);
+    if (fr != FR_OK)
+    {
+        BK_LOGE(TAG, "f_unmount failed:%d\r\n", fr);
+        return BK_FAIL;
+    }
+    else
+    {
+        BK_LOGI(TAG, "f_unmount OK!\r\n");
+    }
 
-	if (pfs)
-	{
-		os_free(pfs);
-		pfs = NULL;
-	}
+    if (pfs)
+    {
+        os_free(pfs);
+        pfs = NULL;
+    }
 
-	return BK_OK;
+    return BK_OK;
 }
 
 static int send_mic_data_to_sd(uint8_t *data, unsigned int len)
 {
-	FRESULT fr;
-	uint32 uiTemp = 0;
+    FRESULT fr;
+    uint32 uiTemp = 0;
 
-	/* write data to file */
-	fr = f_write(&mic_file, (void *)data, len, &uiTemp);
-	if (fr != FR_OK) {
-		BK_LOGE(TAG, "write %s fail.\r\n", mic_file_name);
-	}
+    /* write data to file */
+    fr = f_write(&mic_file, (void *)data, len, &uiTemp);
+    if (fr != FR_OK)
+    {
+        BK_LOGE(TAG, "write %s fail.\r\n", mic_file_name);
+    }
 
-	return uiTemp;
+    return uiTemp;
 }
 
 bk_err_t audio_record_to_sdcard_start(char *file_name, uint32_t samp_rate)
@@ -103,9 +102,11 @@ bk_err_t audio_record_to_sdcard_start(char *file_name, uint32_t samp_rate)
 
     aud_intf_drv_setup_t aud_intf_drv_setup = DEFAULT_AUD_INTF_DRV_SETUP_CONFIG();
     aud_intf_mic_setup_t aud_intf_mic_setup = DEFAULT_AUD_INTF_MIC_SETUP_CONFIG();
+    aud_intf_mic_setup.mic_gain = 0x3f;
 
     ret = tf_mount();
-    if (ret != BK_ERR_AUD_INTF_OK) {
+    if (ret != BK_ERR_AUD_INTF_OK)
+    {
         BK_LOGE(TAG, "tfcard mount fail, ret:%d\n", ret);
         goto fail;
     }
@@ -113,42 +114,47 @@ bk_err_t audio_record_to_sdcard_start(char *file_name, uint32_t samp_rate)
     /*open file to save pcm data */
     sprintf(mic_file_name, "1:/%s", file_name);
     fr = f_open(&mic_file, mic_file_name, FA_CREATE_ALWAYS | FA_WRITE);
-    if (fr != FR_OK) {
+    if (fr != FR_OK)
+    {
         BK_LOGE(TAG, "open %s fail\n", mic_file_name);
         goto fail;
     }
 
     aud_intf_drv_setup.aud_intf_tx_mic_data = send_mic_data_to_sd;
     ret = bk_aud_intf_drv_init(&aud_intf_drv_setup);
-    if (ret != BK_ERR_AUD_INTF_OK) {
+    if (ret != BK_ERR_AUD_INTF_OK)
+    {
         BK_LOGE(TAG, "bk_aud_intf_drv_init fail, ret:%d\n", ret);
         goto fail;
     }
 
     ret = bk_aud_intf_set_mode(AUD_INTF_WORK_MODE_GENERAL);
-    if (ret != BK_ERR_AUD_INTF_OK) {
+    if (ret != BK_ERR_AUD_INTF_OK)
+    {
         BK_LOGE(TAG, "bk_aud_intf_set_mode fail, ret:%d\n", ret);
         goto fail;
     }
 
-    //aud_intf_mic_setup.mic_chl = AUD_INTF_MIC_CHL_MIC1;
+    // aud_intf_mic_setup.mic_chl = AUD_INTF_MIC_CHL_MIC1;
     aud_intf_mic_setup.samp_rate = samp_rate;
-    //aud_intf_mic_setup.mic_type = AUD_INTF_MIC_TYPE_UAC;
+    // aud_intf_mic_setup.mic_type = AUD_INTF_MIC_TYPE_UAC;
     aud_intf_mic_setup.frame_size = 640;
-    //aud_intf_mic_setup.mic_gain = 0x2d;
+    // aud_intf_mic_setup.mic_gain = 0x2d;
     ret = bk_aud_intf_mic_init(&aud_intf_mic_setup);
-    if (ret != BK_ERR_AUD_INTF_OK) {
+    if (ret != BK_ERR_AUD_INTF_OK)
+    {
         BK_LOGE(TAG, "bk_aud_intf_mic_init fail, ret:%d\n", ret);
         goto fail;
     }
 
     ret = bk_aud_intf_mic_start();
-    if (ret != BK_ERR_AUD_INTF_OK) {
+    if (ret != BK_ERR_AUD_INTF_OK)
+    {
         BK_LOGE(TAG, "bk_aud_intf_mic_start fail, ret:%d\n", ret);
         goto fail;
     }
 
-	return BK_OK;
+    return BK_OK;
 
 fail:
 
@@ -165,24 +171,28 @@ fail:
 
 bk_err_t audio_record_to_sdcard_stop(void)
 {
-	bk_err_t ret;
+    bk_err_t ret;
     ret = bk_aud_intf_mic_stop();
-    if (ret != BK_ERR_AUD_INTF_OK) {
+    if (ret != BK_ERR_AUD_INTF_OK)
+    {
         BK_LOGE(TAG, "bk_aud_intf_mic_stop fail, ret:%d\n", ret);
     }
 
     ret = bk_aud_intf_mic_deinit();
-    if (ret != BK_ERR_AUD_INTF_OK) {
+    if (ret != BK_ERR_AUD_INTF_OK)
+    {
         BK_LOGE(TAG, "bk_aud_intf_mic_deinit fail, ret:%d\n", ret);
     }
 
     ret = bk_aud_intf_set_mode(AUD_INTF_WORK_MODE_NULL);
-    if (ret != BK_ERR_AUD_INTF_OK) {
+    if (ret != BK_ERR_AUD_INTF_OK)
+    {
         BK_LOGE(TAG, "bk_aud_intf_set_mode fail, ret:%d\n", ret);
     }
 
     ret = bk_aud_intf_drv_deinit();
-    if (ret != BK_ERR_AUD_INTF_OK) {
+    if (ret != BK_ERR_AUD_INTF_OK)
+    {
         BK_LOGE(TAG, "bk_aud_intf_drv_deinit fail, ret:%d\n", ret);
     }
 
@@ -193,4 +203,3 @@ bk_err_t audio_record_to_sdcard_stop(void)
 
     return BK_OK;
 }
-
