ade017b111f7eaa2f58b69e00db1159023ab3b99
Do-EE2-IoT <do30032003@gmail.com>
2026-05-28 08:46:28 +0000

    i2s dma

--- projects/media/audio_play_sdcard_mp3_music/main/audio_play.c
+++ projects/media/audio_play_sdcard_mp3_music/main/audio_play.c
@@ -30,6 +30,9 @@
 #define PCM_SIZE_MAX		(MAX_NSAMP * MAX_NCHAN * MAX_NGRAN)
 #define I2S_TX_RING_BUFFER_SIZE (16384 * 4)
 #define I2S_MP3_DECODE_STACK_SIZE (4096)
+#define I2S_DMA_SEM_MAX_COUNT 8
+#define I2S_RB_LOW_WATERMARK_SIZE (I2S_TX_RING_BUFFER_SIZE / 4)
+#define I2S_FRAME_LOG_INTERVAL 32
 
 
 typedef struct {
@@ -47,16 +50,25 @@ typedef struct {
 
     RingBufferContext *i2s_tx_rb;
     beken_thread_t decode_thread;
+    beken_semaphore_t i2s_dma_sem;
     bool decode_thread_running;
+    bool i2s_hw_inited;
     bool i2s_started;
+    bool i2s_dma_sem_inited;
+    uint32_t i2s_frame_index;
+    beken_time_t last_i2s_frame_ts_ms;
+    volatile uint32_t i2s_dma_event_count;
+    volatile uint32_t i2s_dma_last_size;
 } audio_play_info_t;
 
 
 static audio_play_info_t *audio_play_info = NULL;
 static FATFS *pfs = NULL;
 
-static bk_err_t audio_play_i2s_hw_start(uint32_t sample_rate, RingBufferContext **tx_rb);
+static bk_err_t audio_play_i2s_hw_init(uint32_t sample_rate, RingBufferContext **tx_rb);
+static bk_err_t audio_play_i2s_dma_start(void);
 static void audio_play_i2s_hw_stop(void);
+static uint32_t audio_decode_fill_until_full(void);
 
 static i2s_samp_rate_t get_i2s_sample_rate(uint32_t mp3_rate)
 {
@@ -190,6 +202,10 @@ static bk_err_t mp3_decode_handler(unsigned int size)
 		audio_play_info->g_readptr = audio_play_info->readBuf;
 	}
 
+	if (ring_buffer_get_free_size(audio_play_info->i2s_tx_rb) < (PCM_SIZE_MAX * 2)) {
+		return BK_ERR_BUSY;
+	}
+
 	int offset = MP3FindSyncWord(audio_play_info->g_readptr, audio_play_info->bytesLeft);
 
 	if (offset < 0) {
@@ -213,16 +229,77 @@ static bk_err_t mp3_decode_handler(unsigned int size)
 		uint32_t pcm_size = audio_play_info->mp3FrameInfo.outputSamps * 2;
 		uint8_t *write_ptr = (uint8_t *)audio_play_info->pcmBuf;
 		uint32_t total_written = 0;
+		uint32_t frame_index = audio_play_info->i2s_frame_index++;
+		uint32_t rb_fill_before;
+		uint32_t rb_free_before;
+		uint32_t rb_fill_after;
+		uint32_t rb_free_after;
+		uint32_t rb_capacity;
+		uint32_t rb_low;
+		bool log_frame;
+		beken_time_t frame_ts_ms = rtos_get_time();
+		beken_time_t frame_delta_ms = 0;
+		beken_time_t write_start_ms;
+		beken_time_t write_end_ms;
+		beken_time_t write_dt_ms;
+		uint32_t loops = 0;
 
+		if (audio_play_info->last_i2s_frame_ts_ms != 0) {
+			frame_delta_ms = frame_ts_ms - audio_play_info->last_i2s_frame_ts_ms;
+		}
+		audio_play_info->last_i2s_frame_ts_ms = frame_ts_ms;
+
+		rb_fill_before = ring_buffer_get_fill_size(audio_play_info->i2s_tx_rb);
+		rb_free_before = ring_buffer_get_free_size(audio_play_info->i2s_tx_rb);
+		rb_capacity = audio_play_info->i2s_tx_rb ? audio_play_info->i2s_tx_rb->capacity : 0;
+		rb_low = (rb_fill_before < I2S_RB_LOW_WATERMARK_SIZE) ? 1 : 0;
+
+		write_start_ms = rtos_get_time();
 		while (total_written < pcm_size) {
 			uint32_t written = ring_buffer_write(audio_play_info->i2s_tx_rb,
 												 write_ptr + total_written,
 												 pcm_size - total_written);
 
-			total_written += written;
-			if (total_written < pcm_size) {
-				rtos_delay_milliseconds(2);
+			loops++;
+			if (written == 0) {
+				return BK_ERR_BUSY;
 			}
+			total_written += written;
+		}
+		write_end_ms = rtos_get_time();
+		write_dt_ms = write_end_ms - write_start_ms;
+		rb_fill_after = ring_buffer_get_fill_size(audio_play_info->i2s_tx_rb);
+		rb_free_after = ring_buffer_get_free_size(audio_play_info->i2s_tx_rb);
+		log_frame = ((frame_index % I2S_FRAME_LOG_INTERVAL) == 0) ||
+					(loops > 1) ||
+					(write_dt_ms > 5);
+
+		if (log_frame) {
+			BK_LOGW(TAG,
+					"[SD_MP3][I2S_FRAME] frame=%lu size=%lu start=%lu end=%lu write_dt=%lu loops=%lu rb_fill_before=%lu rb_free_before=%lu rb_fill_after=%lu rb_free_after=%lu rb_capacity=%lu low=%lu\n",
+					(unsigned long)frame_index,
+					(unsigned long)pcm_size,
+					(unsigned long)write_start_ms,
+					(unsigned long)write_end_ms,
+					(unsigned long)write_dt_ms,
+					(unsigned long)loops,
+					(unsigned long)rb_fill_before,
+					(unsigned long)rb_free_before,
+					(unsigned long)rb_fill_after,
+					(unsigned long)rb_free_after,
+					(unsigned long)rb_capacity,
+					(unsigned long)rb_low);
+			BK_LOGW(TAG,
+					"[SD_MP3][FRAME_TIMING] frame=%lu frame_start=%lu delta_prev=%lu total_dt=%lu pcm_size=%lu bytes_left=%d sr=%d ch=%d output_samps=%d\n",
+					(unsigned long)frame_index,
+					(unsigned long)frame_ts_ms,
+					(unsigned long)frame_delta_ms,
+					(unsigned long)(write_end_ms - frame_ts_ms),
+					(unsigned long)pcm_size,
+					audio_play_info->bytesLeft,
+					audio_play_info->mp3FrameInfo.samprate,
+					audio_play_info->mp3FrameInfo.nChans,
+					audio_play_info->mp3FrameInfo.outputSamps);
 		}
 	}
 
@@ -242,6 +319,9 @@ bk_err_t audio_play_sdcard_mp3_music_stop(void)
 		int wait_count = 0;
 
 		audio_play_info->decode_thread_running = false;
+		if (audio_play_info->i2s_dma_sem_inited) {
+			rtos_set_semaphore(&audio_play_info->i2s_dma_sem);
+		}
 		while (audio_play_info->decode_thread != NULL && wait_count < 200) {
 			rtos_delay_milliseconds(10);
 			wait_count++;
@@ -256,6 +336,15 @@ bk_err_t audio_play_sdcard_mp3_music_stop(void)
 	if (audio_play_info->i2s_started) {
 		audio_play_i2s_hw_stop();
 		audio_play_info->i2s_started = false;
+		audio_play_info->i2s_hw_inited = false;
+	} else if (audio_play_info->i2s_hw_inited) {
+		audio_play_i2s_hw_stop();
+		audio_play_info->i2s_hw_inited = false;
+	}
+
+	if (audio_play_info->i2s_dma_sem_inited) {
+		rtos_deinit_semaphore(&audio_play_info->i2s_dma_sem);
+		audio_play_info->i2s_dma_sem_inited = false;
 	}
 
 	audio_play_info->bytesLeft = 0;
@@ -290,10 +379,44 @@ bk_err_t audio_play_sdcard_mp3_music_stop(void)
 
 static int i2s_tx_data_callback(uint32_t size)
 {
+	if (audio_play_info && audio_play_info->i2s_dma_sem_inited) {
+		audio_play_info->i2s_dma_event_count++;
+		audio_play_info->i2s_dma_last_size = size;
+		rtos_set_semaphore(&audio_play_info->i2s_dma_sem);
+	}
+
 	return size;
 }
 
-static bk_err_t audio_play_i2s_hw_start(uint32_t sample_rate, RingBufferContext **tx_rb)
+static uint32_t audio_decode_fill_until_full(void)
+{
+	uint32_t decoded_frames = 0;
+
+	while (audio_play_info && audio_play_info->decode_thread_running) {
+		bk_err_t ret;
+
+		if (audio_play_info->mp3_file_is_empty) {
+			break;
+		}
+
+		ret = mp3_decode_handler(0);
+		if (ret == BK_OK) {
+			decoded_frames++;
+			continue;
+		}
+
+		if (ret == BK_ERR_BUSY) {
+			break;
+		}
+
+		rtos_delay_milliseconds(1);
+		break;
+	}
+
+	return decoded_frames;
+}
+
+static bk_err_t audio_play_i2s_hw_init(uint32_t sample_rate, RingBufferContext **tx_rb)
 {
 	bk_err_t ret;
 	i2s_config_t i2s_config = DEFAULT_I2S_CONFIG();
@@ -338,12 +461,26 @@ static bk_err_t audio_play_i2s_hw_start(uint32_t sample_rate, RingBufferContext 
 		BK_LOGW(TAG, "bk_i2s_set_samp_rate fail, ret:%d\n", ret);
 	}
 
+	if (audio_play_info) {
+		audio_play_info->i2s_hw_inited = true;
+	}
+
+	return BK_OK;
+}
+
+static bk_err_t audio_play_i2s_dma_start(void)
+{
+	bk_err_t ret;
+
 	ret = bk_i2s_start();
 	if (ret != BK_OK) {
 		BK_LOGE(TAG, "bk_i2s_start fail, ret:%d\n", ret);
 		bk_i2s_chl_deinit(I2S_CHANNEL_1, I2S_TXRX_TYPE_TX);
 		bk_i2s_deinit();
 		bk_i2s_driver_deinit();
+		if (audio_play_info) {
+			audio_play_info->i2s_hw_inited = false;
+		}
 		return ret;
 	}
 
@@ -371,17 +508,16 @@ static void audio_decode_thread(void *arg)
 			break;
 		}
 
-		if (mp3_decode_handler(0) != BK_OK) {
-			rtos_delay_milliseconds(5);
-			continue;
-		}
-
-		rtos_delay_milliseconds(2);
+		audio_decode_fill_until_full();
+		rtos_get_semaphore(&audio_play_info->i2s_dma_sem, BEKEN_WAIT_FOREVER);
 	}
 
 	if (audio_play_info) {
 		audio_play_info->decode_thread_running = false;
 		audio_play_info->decode_thread = NULL;
+		if (audio_play_info->i2s_dma_sem_inited) {
+			rtos_set_semaphore(&audio_play_info->i2s_dma_sem);
+		}
 	}
 
 	BK_LOGI(TAG, "MP3 decode thread exit\n");
@@ -392,6 +528,7 @@ bk_err_t audio_play_sdcard_mp3_music_start(char *file_name)
 {
 	bk_err_t ret = BK_OK;
     uint32 uiTemp = 0;
+	uint32_t prefill_frames = 0;
 	char tag_header[10];
 	int tag_size = 0;
 
@@ -470,17 +607,38 @@ bk_err_t audio_play_sdcard_mp3_music_start(char *file_name)
         f_lseek(&audio_play_info->mp3file, 0);
     }
 
-	ret = audio_play_i2s_hw_start(48000U, &audio_play_info->i2s_tx_rb);
+	ret = rtos_init_semaphore(&audio_play_info->i2s_dma_sem, I2S_DMA_SEM_MAX_COUNT);
+	if (ret != BK_OK) {
+		BK_LOGE(TAG, "init i2s dma semaphore fail, ret:%d\n", ret);
+		goto fail;
+	}
+	audio_play_info->i2s_dma_sem_inited = true;
+
+	ret = audio_play_i2s_hw_init(48000U, &audio_play_info->i2s_tx_rb);
 	if (ret != BK_OK) {
 		goto fail;
 	}
 
 	audio_play_info->g_readptr = audio_play_info->readBuf;
-	ret = mp3_decode_handler(audio_play_info->mp3FrameInfo.outputSamps * 2);
-    if (ret < 0) {
-        BK_LOGE(TAG, "mp3_decode_handler fail, ret:%d\n", ret);
-        goto fail;
-    }
+	while (!audio_play_info->mp3_file_is_empty) {
+		ret = mp3_decode_handler(0);
+		if (ret == BK_OK) {
+			prefill_frames++;
+			continue;
+		}
+
+		if (ret == BK_ERR_BUSY) {
+			break;
+		}
+
+		BK_LOGE(TAG, "mp3_decode_handler fail during prefill, ret:%d\n", ret);
+		goto fail;
+	}
+
+	if (prefill_frames == 0) {
+		BK_LOGE(TAG, "prefill i2s ring buffer failed\n");
+		goto fail;
+	}
 
 	ret = bk_i2s_set_samp_rate(I2S_SAMP_RATE_48000);
 	if (ret != BK_OK) {
@@ -490,6 +648,10 @@ bk_err_t audio_play_sdcard_mp3_music_start(char *file_name)
 	BK_LOGI(TAG, "I2S group2 MP3 playback started, i2s_rate=48000 mp3_rate=%d channels=%d\n",
 			audio_play_info->mp3FrameInfo.samprate,
 			audio_play_info->mp3FrameInfo.nChans);
+	BK_LOGI(TAG, "I2S DMA prefilled, frames=%lu fill=%lu free=%lu\n",
+			(unsigned long)prefill_frames,
+			(unsigned long)ring_buffer_get_fill_size(audio_play_info->i2s_tx_rb),
+			(unsigned long)ring_buffer_get_free_size(audio_play_info->i2s_tx_rb));
 
 	audio_play_info->decode_thread_running = true;
 	audio_play_info->decode_thread = NULL;
@@ -505,6 +667,15 @@ bk_err_t audio_play_sdcard_mp3_music_start(char *file_name)
 		goto fail;
 	}
 
+	ret = audio_play_i2s_dma_start();
+	if (ret != BK_OK) {
+		audio_play_info->decode_thread_running = false;
+		if (audio_play_info->i2s_dma_sem_inited) {
+			rtos_set_semaphore(&audio_play_info->i2s_dma_sem);
+		}
+		goto fail;
+	}
+
     return BK_OK;
 
 fail:
@@ -513,4 +684,3 @@ fail:
 
 	return BK_FAIL;
 }
-
